f40fd7b92ce51ec3f9eb26f997aa4794eecefa28
pereza <aperez@predictia.es>
2023-05-22 14:33:25 +0200

    data generator final version

--- deepr/main.py
+++ deepr/main.py
@@ -1,3 +1,5 @@
+from torch.utils.data import DataLoader
+
 from deepr.data.configuration import DataConfiguration
 from deepr.data.generator import DataGenerator
 from deepr.utilities.logger import get_logger
@@ -27,27 +29,38 @@ class MainPipeline:
         DataGenerator
             The initialized DataGenerator object.
         """
-        data_generator = self.initialize_data()
-        return data_generator
+        logger.info("Prepare DataLoader object for modeling")
+        data_loader = self.prepare_dataloader()
+        for batch in data_loader:
+            features, labels = batch
+        return data_loader
 
-    def initialize_data(self):
+    def prepare_dataloader(self):
         """
-        Initialize the data for the pipeline.
+        Initialize the data_loader for the pipeline.
 
         Returns
         -------
-        DataGenerator
-            The initialized DataGenerator object.
+        DataLoader
+            The initialized DataLoader object.
         """
         data_configuration = DataConfiguration(self.configuration["data_configuration"])
+        logger.info("Get features from data_configuration dictionary")
         features_collection = data_configuration.get_features()
+        logger.info("Get label from data_configuration dictionary")
         label_collection = data_configuration.get_label()
+        logger.info("Define the DataGenerator object")
         data_generator = DataGenerator(
             features_collection,
             label_collection,
             data_configuration.common_configuration["batch_size"],
         )
-        return data_generator
+        logger.info("Define the DataLoader object")
+        data_loader = DataLoader(
+            dataset=data_generator,
+            batch_size=data_configuration.common_configuration["batch_size"],
+        )
+        return data_loader
 
 
 if __name__ == "__main__":
