f92fc892de9f73c66af9f186e43728ae389843c8
Mario Santa Cruz Lopez <santacruzm@predictia.es>
2023-05-25 12:14:12 +0200

    Explainable Docs

--- deepr/model/ddpm.py
+++ deepr/model/ddpm.py
@@ -9,16 +9,37 @@ from deepr.model.utils import gather
 
 
 class DenoiseDiffusion:
+    """Denoising Diffuision Probabilistic Model.
+
+    Attributes
+    ----------
+    eps_model : nn.Module
+        neural network to predict the noise added in the forward process
+    beta : torch.Tensor
+        variance of the Isotropic Gaussian distribution used to sample the noise in the
+        forward process.
+    alpha : torch.Tensor
+        1 - beta. These values are useful to sample an arbitrary step in closed form.
+    alpha_bar: torch.Tensor
+        cumulative product of alpha. 1 - alpha_bar is the variance of the noise to
+        sample an arbitrary step.
+    n_steps : int
+        number of steps of the diffusion process
+    """
+
     def __init__(self, eps_model: nn.Module, n_steps: int, device: torch.device):
         """
         Initialize the Denoise Diffusion Model with the provided parameters.
 
-        Args:
-        ----
-            eps_model (nn.Module): The eps model module.
-            n_steps (int): The number of steps of the diffusion process.
-            device (torch.device): The device to use.
-
+        Arguments
+        ---------
+            eps_model : nn.Module
+                The Neural Network that will predict the noise added in the forward
+                process.
+            n_steps : int
+                The number of steps of the diffusion process.
+            device : torch.device
+                The device to use. Options: "cpu", 0, 1, 2, ...
         """
         super().__init__()
         self.eps_model = eps_model
@@ -27,11 +48,27 @@ class DenoiseDiffusion:
         self.alpha = 1.0 - self.beta
         self.alpha_bar = torch.cumprod(self.alpha, dim=0)
         self.n_steps = n_steps
-        self.sigma2 = self.beta
 
     def q_xt_x0(
         self, x0: torch.Tensor, t: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Auxiliary function.
+
+        This method computes the mean and variance of Gaussian Distribution to sample xt
+        from x0.
+
+        Arguments
+        ---------
+            x0 : torch.Tensor
+                Matrix from initial data distribution.
+            t : torch.Tensor
+                time step at which to sample xt.
+
+        Returns
+        -------
+            torch.Tensor : the mean of the distribution = x0 * sqrt(alpha_bar[t])
+            torch.Tensor : the variance of the distribution = 1 - alpha_bar[t]
+        """
         mean = gather(self.alpha_bar, t) ** 0.5 * x0
         var = 1 - gather(self.alpha_bar, t)
 
@@ -40,6 +77,25 @@ class DenoiseDiffusion:
     def q_sample(
         self, x0: torch.Tensor, t: torch.Tensor, eps: Optional[torch.Tensor] = None
     ):
+        """q(xt|x0).
+
+        This method samples xt from x0, using the closed form of q(xt|x0) ~ N(m, vI)
+        with m = x0 * sqrt(alpha_bar[t]) and v = 1 - alpha_bar[t].
+
+        Arguments
+        ---------
+            x0 : torch.Tensor
+                Matrix from initial data distribution.
+            t : torch.Tensor
+                time step at which to sample xt.
+            eps : Optional[torch.Tensor], optional
+                Noise to be added. Defaults to None, meaining that the noise is sample
+                here.
+
+        Returns
+        -------
+            torch.Tensor: the matrix corresponding to xt
+        """
         if eps is None:
             eps = torch.randn_like(x0)
 
@@ -47,16 +103,45 @@ class DenoiseDiffusion:
         return mean + (var**0.5) * eps
 
     def p_sample(self, xt: torch.Tensor, t: torch.Tensor):
+        """p(xt-1|xt).
+
+        This method samples xt-1 from xt, using the current NN predictions of the noise.
+
+        Arguments
+        ---------
+            xt : torch.Tensor
+                Matrix from initial data distribution.
+            t : torch.Tensor
+                time step at which to sample xt.
+
+        Returns
+        -------
+            torch.Tensor: the matrix corresponding to xt-1 according to the current NN.
+        """
         eps_theta = self.eps_model(xt, t)
         alpha_bar = gather(self.alpha_bar, t)
         alpha = gather(self.alpha, t)
         eps_coef = (1 - alpha) / (1 - alpha_bar) ** 0.5
         mean = 1 / (alpha**0.5) * (xt - eps_coef * eps_theta)
-        var = gather(self.sigma2, t)
+        var = gather(self.beta, t)
         eps = torch.randn(xt.shape, device=xt.device)
         return mean + (var**0.5) * eps
 
     def loss(self, x0: torch.Tensor, noise: Optional[torch.Tensor] = None):
+        """Compute the simplified Loss for training the Diffusion Probabilistic Model.
+
+        Arguments
+        ---------
+            xt : torch.Tensor
+                Matrix from initial data distribution.
+            noise : Optional[torch.Tensor], optional
+                Noise added during the process. Defaults to None.
+
+        Returns
+        -------
+            torch.Tensor: mean squared error between the noised added in the forward
+                process and the noise predicted by the neural network.
+        """
         batch_size = x0.shape[0]
         # Get random t for each sample in the batch
         t = torch.randint(
