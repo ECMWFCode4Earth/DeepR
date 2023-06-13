55b2279ed2ef0deab7b139f04e1fe5820b2b6376
pereza <aperez@predictia.es>
2023-06-13 13:01:12 +0200

    separating the static data computation to other module inside data

--- deepr/data/static/climatology.py
+++ deepr/data/static/climatology.py
@@ -1,11 +1,8 @@
-import cartopy.crs as ccrs
-import cartopy.feature as cf
-import matplotlib.pyplot as plt
 import pandas
 import xarray
 
 
-def compute_statistic_by_time_group(
+def compute_climatology_by_time_group(
     data_files: list, group_by: list, output_directory: str
 ):
     """
@@ -73,56 +70,10 @@ def compute_statistic_by_time_group(
     return mean_by_group, std_by_group
 
 
-def plot_data(ds: xarray.Dataset):
-    projection = ccrs.Robinson()
-    crs = ccrs.PlateCarree()
-
-    plt.figure(figsize=(8, 8))
-    ax = plt.axes(projection=projection, frameon=True)
-    gl = ax.gridlines(
-        crs=crs,
-        draw_labels=True,
-        linewidth=0.6,
-        color="gray",
-        alpha=0.5,
-        linestyle="-.",
-    )
-    gl.xlabel_style = {"size": 7}
-    gl.ylabel_style = {"size": 7}
-
-    ax.add_feature(cf.COASTLINE.with_scale("50m"), lw=0.5)
-
-    cbar_kwargs = {
-        "orientation": "horizontal",
-        "shrink": 0.6,
-        "pad": 0.05,
-        "aspect": 40,
-        "label": "Temperature",
-    }
-
-    lon_min = -50
-    lon_max = 60
-    lat_min = 10
-    lat_max = 80
-    ax.set_extent([lon_min, lon_max, lat_min, lat_max], crs=crs)
-
-    ds.mean("time")["t2m"].plot(
-        ax=ax,
-        cmap="YlOrBr",
-        transform=ccrs.PlateCarree(),
-        cbar_kwargs=cbar_kwargs,
-        vmin=250,
-        vmax=300,
-    )
-
-    plt.title("Mean temperature January 2018 - CERRA")
-    plt.close()
-
-
 if __name__ == "__main__":
     import glob
 
-    compute_statistic_by_time_group(
+    compute_climatology_by_time_group(
         glob.glob("../../tests/data/labels/*.nc"),
         ["hour", "day", "month"],
         output_directory="/tmp",
