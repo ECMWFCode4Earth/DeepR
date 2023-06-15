d7925f3856b0d5769175d2ec0e9c7b432d9d4cae
pereza <aperez@predictia.es>
2023-06-15 12:59:43 +0200

    accelerate working

---
+++ deepr/cli.py
@@ -0,0 +1,43 @@
+import click
+from pathlib import Path
+
+from deepr.workflow import MainPipeline
+from deepr.utilities.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+@click.group()
+def launcher():
+    pass
+
+
+@launcher.command("train_model")
+@click.option(
+    "-c",
+    "--configuration_yaml",
+    help="File configuring the parameters for training the model",
+    type=str,
+)
+def train_model(configuration_yaml: str) -> None:
+    """
+    Train a model.
+
+    Parameters
+    ----------
+    configuration_yaml : str
+        File configuring the parameters for training the model.
+        Default is "{main_folder}/resources/configuration/config.yml".
+
+    Returns
+    -------
+    None
+        This function does not return any value.
+
+    """
+    logger.info(
+        f"Starting the process of training a model with the "
+        f"configuration specified at {configuration_yaml}"
+    )
+    MainPipeline(configuration_file=Path(configuration_yaml)).run_pipeline()
+    logger.info("Process finished")
