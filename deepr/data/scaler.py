102b5554a98a08d4cd83d210e8a314ab5bb7f3c1
pereza <aperez@predictia.es>
2023-06-20 11:58:03 +0200

    Adding cache directory for standardization

--- deepr/data/scaler.py
+++ deepr/data/scaler.py
@@ -52,6 +52,7 @@ class XarrayStandardScaler:
             The dataset containing the standard deviation values of the parameters.
         """
 
+        @self.create_memory().cache
         def compute_parameters():
             dataset = xarray.open_mfdataset(
                 [file.to_path() for file in self.files.collection]
