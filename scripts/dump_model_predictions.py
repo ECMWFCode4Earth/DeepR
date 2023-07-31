9def4c688eb915708a443d028f110a4006c22f32
Mario Santa Cruz Lopez <santacruzm@predictia.es>
2023-07-31 14:36:29 +0200

    add dump prediction cli

---
+++ scripts/dump_model_predictions.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from deepr.workflow import MainPipeline
+
+
+def main(cfg_path: Path):
+    main_pipeline = MainPipeline(cfg_path)
+    main_pipeline.generate_predictions()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        prog="dump_model_predictions.py",
+        description="Dump predictions from Super Resolution model",
+    )
+    parser.add_argument(
+        "--cfg_path",
+        default="../resources/configuration_predictions.yml",
+        type=Path,
+        help="Path to the configuration file.",
+    )
+
+    args = parser.parse_args()
+    main(cfg_path=Path(args.cfg_path))
